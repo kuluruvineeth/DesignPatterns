ff852af220ffb8653b5390690668c45302a44a1f
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-10 12:40:04 +0530

    Add project files.

---
+++ LiskovSubstitution/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace LiskovSubstitution
+{
+
+    public class Rectangle
+    {
+        //public int Width {get;set;}
+        //public int Height {get;set;}
+
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+            
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        /*public new int Width
+        {
+            set
+            {
+                base.Width = base.Height = value;
+            }
+        }
+
+        public new int Height
+        {
+            set { base.Height = base.Width = value; }
+        }*/
+
+        public override int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public override int Height
+        {
+            set { base.Height = base.Width = value; }
+        }
+    }
+    internal class Program
+    {
+        static public int Area(Rectangle r) => r.Width * r.Height;
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2, 3);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+
+            //Should be able to substitute a base type for a subtype
+            Rectangle sq = new Square();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+        }
+    }
+}
